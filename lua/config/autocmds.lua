2599323124523e9511b523693ff49419b0ebcdf1
Abbe Azale <abbeazale98@gmail.com>
2025-09-20 00:22:46 -0700

    added some yanking things

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,9 +3,9 @@
 -- Add any additional autocmds here
 
 -- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
